bc201661b3071c25ab09982ad486f7408f7a273f
Venkaiah Chowdary Koneru <koneru.chowdary@gmail.com>
2012-11-09 18:07:46 +0800

    adding category data for unit-test

--- interview-faq/src/test/resources/test-data.sql
+++ interview-faq/src/test/resources/test-data.sql
@@ -1,1 +1,6 @@
-INSERT INTO user (id, version, deleted, created_date, modified_date, name, email, password, role) VALUES  (1, 0, 0, '2012-06-01 16:02:49', NULL, 'Administrator', 'admin@faqmasters.com', '6f87c6a32d765bbfb7113732e046a71d9537cbea3cd2c145c5cf08c95997b88b', 'ADMIN');
+INSERT INTO user (id, created_date, deleted, modified_date, version, name, email, password, role) VALUES (1, '2012-06-01 16:02:49', 0, NULL, 0, 'Administrator', 'admin@faqmasters.com', '6f87c6a32d765bbfb7113732e046a71d9537cbea3cd2c145c5cf08c95997b88b', 'ADMIN');
+INSERT INTO user (id, created_date, deleted, modified_date, version, email, name, password, role) VALUES (2, '2012-11-09 17:51:34', 0, NULL, 0, 'venky@gmail.com', 'Venky', '573027f536ca557e79b8e8247db75dad96a0aa50dc150c4ff3d34a06b98562cd', 'ADMIN');
+
+INSERT INTO category (id, created_date, deleted, modified_date, version, categoryDescription, name) VALUES (1, '2012-11-09 17:52:15', 0, NULL, 0, 'Java Category', 'Java');
+INSERT INTO category (id, created_date, deleted, modified_date, version, categoryDescription, name) VALUES (2, '2012-11-09 17:52:36', 0, NULL, 0, 'php category', 'PHP');
+INSERT INTO category (id, created_date, deleted, modified_date, version, categoryDescription, name) VALUES (3, '2012-11-09 17:53:05', 0, NULL, 0, 'category for .NET', '.NET');
