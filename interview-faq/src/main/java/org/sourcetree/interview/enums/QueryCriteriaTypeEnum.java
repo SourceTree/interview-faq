3ddab1ec9b3b8606b446d6ab07d3f99eab147c80
koneru9999 <koneru.chowdary@gmail.com>
2012-11-10 18:06:29 +0800

    query flags

---
+++ interview-faq/src/main/java/org/sourcetree/interview/enums/QueryCriteriaTypeEnum.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2012, Source Tree, All Rights Reserved
+ * 
+ * QueryCriteriaTypeEnum.java
+ * Modification History
+ * *************************************************************
+ * Date				Author						Comment
+ * Nov 10, 2012		Venkaiah Chowdary Koneru	Created
+ * *************************************************************
+ */
+package org.sourcetree.interview.enums;
+
+/**
+ * Java 5 enumeration of Query Criteria Type Enum types. Intended for using it
+ * in the DAO query criteria.
+ * 
+ * @author Venkaiah Chowdary Koneru
+ * 
+ */
+public enum QueryCriteriaTypeEnum
+{
+	/**
+	 * 
+	 */
+	DELETED,
+
+	/**
+	 * 
+	 */
+	IGNORE_CASE
+}
