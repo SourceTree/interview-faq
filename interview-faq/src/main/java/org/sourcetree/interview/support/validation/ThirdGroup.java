e622db49dc878549c75cfa1858ab54b60a65f58b
koneru9999 <koneru.chowdary@gmail.com>
2012-11-17 15:20:49 +0800

    new group for validation

---
+++ interview-faq/src/main/java/org/sourcetree/interview/support/validation/ThirdGroup.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2012, Source Tree, All Rights Reserved
+ * 
+ * ThirdGroup.java
+ * Modification History
+ * *************************************************************
+ * Date				Author						Comment
+ * 17-Nov-2012		Venkaiah Chowdary Koneru	Created
+ * *************************************************************
+ */
+package org.sourcetree.interview.support.validation;
+
+/**
+ * Common group for all the thirds level errors.
+ * 
+ * @author Venkaiah Chowdary Koneru
+ */
+public interface ThirdGroup
+{
+
+}
